a4f235ddd726e852013e77731861fa1c27f921ee
ferriman <ferri.fc@gmail.com>
2014-07-27 19:04:26 +0200

    without potentiometer running example

--- arduino/ServoTimelapse.ino
+++ arduino/ServoTimelapse.ino
@@ -7,9 +7,9 @@
  http://arduino.cc/en/Tutorial/Sweep
 */ 
 
-#include <Servo.h> 
+//#include <Servo.h> 
  
-Servo myservo;  // create servo object to control a servo 
+//Servo myservo;  // create servo object to control a servo 
                 // twelve servo objects can be created on most boards
  
 int pos = 0;    // variable to store the servo position 
@@ -17,13 +17,13 @@ int potentiometerValue=0;
 int PinReading;
 int lenMicroSecondsOfPeriod = 20 * 1000; // 20 milliseconds (ms)
 int lenMicroSecondsOfPulse = 1.8 * 1000; // 1.0 ms is 0 degrees
-Servo srv;
+//Servo srv;
 
 void setup() 
 { 
   Serial.begin(9600);
   pinMode(11,OUTPUT);  // attaches the servo on pin 11 to the servo object
-  srv.attach(11);
+  //srv.attach(11);
   
 } 
  
@@ -32,18 +32,18 @@ void loop()
   //myservo.write(180);
   PinReading=analogRead(3);
   delay(10);
-  //potentiometerValue=map(PinReading,0,1023,0,180);
-  //potentiometerValue=constrain(potentiometerValue,0,180);
+  potentiometerValue=map(PinReading,0,1023,0,180);
+  potentiometerValue=constrain(potentiometerValue,0,180);
   //Serial.print(potentiometerValue);
   //Serial.print("\n");
   int pulsetime(1600000);
   digitalWrite(11,HIGH);
-  //delayMicroseconds(pulsetime);
-  delayMicroseconds(1600000);
+  delayMicroseconds(pulsetime);
+  //delayMicroseconds(1600000);
   digitalWrite(11,LOW);
-  delayMicroseconds(1600000);
-  delay(PinReading);
+  //delayMicroseconds(1600000);
+  //delay(PinReading);
   //srv.write(potentiometerValue);
-  //delay(50);
+  delay(1000);
 }
 
