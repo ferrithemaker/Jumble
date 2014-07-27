7dde9a197a0f3c17f289bc48bfa5a9ad6a722362
ferriman <ferri.fc@gmail.com>
2014-07-27 18:37:20 +0200

    potentiometer added to control timelapse velocity- testing

--- arduino/ServoTimelapse.ino
+++ arduino/ServoTimelapse.ino
@@ -13,19 +13,37 @@ Servo myservo;  // create servo object to control a servo
                 // twelve servo objects can be created on most boards
  
 int pos = 0;    // variable to store the servo position 
- 
+int potentiometerValue=0;
+int PinReading;
+int lenMicroSecondsOfPeriod = 20 * 1000; // 20 milliseconds (ms)
+int lenMicroSecondsOfPulse = 1.8 * 1000; // 1.0 ms is 0 degrees
+Servo srv;
+
 void setup() 
 { 
-  pinMode(11,OUTPUT);  // attaches the servo on pin 9 to the servo object 
+  Serial.begin(9600);
+  pinMode(11,OUTPUT);  // attaches the servo on pin 11 to the servo object
+  srv.attach(11);
+  
 } 
  
 void loop() 
 { 
   //myservo.write(180);
+  PinReading=analogRead(3);
+  delay(10);
+  //potentiometerValue=map(PinReading,0,1023,0,180);
+  //potentiometerValue=constrain(potentiometerValue,0,180);
+  //Serial.print(potentiometerValue);
+  //Serial.print("\n");
   int pulsetime(1600000);
   digitalWrite(11,HIGH);
-  delayMicroseconds(pulsetime);
+  //delayMicroseconds(pulsetime);
+  delayMicroseconds(1600000);
   digitalWrite(11,LOW);
-  delay(1000);
-} 
+  delayMicroseconds(1600000);
+  delay(PinReading);
+  //srv.write(potentiometerValue);
+  //delay(50);
+}
 
