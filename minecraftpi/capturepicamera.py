deecb324141e25d1c08dcc0be89a5e662634733e
ferran <ferri.fc@gmail.com>
2014-11-04 22:39:21 +0100

    pi camera capture image to be used with minecraftpi image renderer

---
+++ minecraftpi/capturepicamera.py
@@ -0,0 +1,15 @@
+import picamera
+import Image
+
+savefile="/home/pi/capt.jpg"
+savefile_t="/home/pi/capt_t.jpg"
+size= 128,128
+camera=picamera.PiCamera()
+camera.start_preview()
+i=raw_input()
+camera.capture(savefile)
+camera.stop_preview()
+im=Image.open(savefile)
+im.thumbnail(size,Image.ANTIALIAS)
+im.save(savefile_t,"JPEG")
+
