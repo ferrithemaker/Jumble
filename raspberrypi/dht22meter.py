18b9eb55e92a0b482a5a2b74461a3a55756c4c2f
ferran <ferri.fc@gmail.com>
2015-01-06 22:57:47 +0100

    firmata to conect with arduino

---
+++ raspberrypi/dht22meter.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# Copyright (c) 2014 Adafruit Industries
+# Author: Tony DiCola
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import sys
+
+import Adafruit_DHT
+import math
+from pyfirmata import Arduino, util, SERVO
+
+
+def arduino_map(x, in_min, in_max, out_min, out_max):
+        return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
+
+# Try to grab a sensor reading.  Use the read_retry method which will retry up
+# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4)
+
+# Note that sometimes you won't get a reading and
+# the results will be null (because Linux can't
+# guarantee the timing of calls to read the sensor).  
+# If this happens try again!
+if humidity is not None and temperature is not None:
+	print '{0:0.1f},{1:0.1f}'.format(humidity, temperature)
+	board=Arduino("/dev/ttyACM0")
+	board.digital[10].mode = SERVO # humidity
+	board.digital[11].mode = SERVO # temp
+	servotemp=arduino_map(math.fabs(temperature),0,45,0,180)
+	servohum=arduino_map(math.fabs(humidity),0,100,0,180)
+	board.digital[10].write(servohum)
+	board.digital[11].write(servotemp)
+else:
+	print 'Failed to get reading. Try again!'
---
+++ raspberrypi/photocellmeter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# Example for RC timing reading for Raspberry Pi
+# Must be used with GPIO 0.3.1a or later - earlier verions
+# are not fast enough!
+
+import RPi.GPIO as GPIO, time, os      
+import math
+from pyfirmata import Arduino, util, SERVO
+import time
+
+board=Arduino("/dev/ttyACM0")
+board.digital[9].mode = SERVO
+
+DEBUG = 1
+GPIO.setmode(GPIO.BCM)
+
+def arduino_map(x, in_min, in_max, out_min, out_max):
+	return (x - in_min) * (out_max - out_min) // (in_max - in_min) + out_min
+
+def RCtime (RCpin):
+        reading = 0
+        GPIO.setup(RCpin, GPIO.OUT)
+        GPIO.output(RCpin, GPIO.LOW)
+        time.sleep(0.1)
+
+        GPIO.setup(RCpin, GPIO.IN)
+        # This takes about 1 millisecond per loop cycle
+        while (GPIO.input(RCpin) == GPIO.LOW):
+                reading += 1
+        return reading
+
+result=RCtime(18);
+if result>1024:
+	result=1024
+if result<0:
+	result=0
+#result=arduino_map(1024-result,0,200,0,120)
+#result=result/4.0
+
+print '%.2f' %(32-math.sqrt(result))
+servoposition=arduino_map(32-math.sqrt(result),0,32,0,180)
+board.digital[9].write(servoposition)
