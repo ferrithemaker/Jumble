cbb31d3cf6af81c45c09744813fbad7e70aab673
ferriman <ferri.fc@gmail.com>
2015-03-17 19:42:45 +0100

    some path improvements

--- pythonscripts/photo_mosaic_generator.py
+++ pythonscripts/photo_mosaic_generator.py
@@ -5,12 +5,19 @@ import sys
 import os.path
 import Image
 import glob
+import shutil
 from pymos.core import build_mosaic
 
-
-filelist= glob.glob("/home/ferran/nexus5-last/DCIM/Camera/*.jpg")
+# variables
+inputpicturefolder='/home/ferran/nexus5-last/DCIM/Camera/*.jpg'
 thumbnail_size = (100, 100)
+thumbspicturefolder='/home/ferran/mosaic/'
+inputpicture='rpi.jpg'
+outputmosaic='rpi_mosaic.png'
 
+filelist= glob.glob(inputpicturefolder)
+shutil.rmtree(thumbspicturefolder)
+os.mkdir(thumbspicturefolder)
 # Loop through all provided arguments
 for i in range(1, len(filelist)):
     try:
@@ -28,17 +35,17 @@ for i in range(1, len(filelist)):
     # Split our original filename into name and extension
     (file_path, extension) = os.path.splitext(filepath)
     file_path_split=file_path.split('/')
-    print 'Converting /home/ferran/mosaic/'+file_path_split[len(file_path_split)-1]+'.jpg'
+    print 'Converting '+thumbspicturefolder+file_path_split[len(file_path_split)-1]+'.jpg'
     
     # Save the thumbnail as "(original_name)_thumb.jpg"
-    image.save('/home/ferran/mosaic/'+file_path_split[len(file_path_split)-1]+'.jpg')
+    image.save(thumbspicturefolder+file_path_split[len(file_path_split)-1]+'.jpg')
 print "Building photo mosaic..."
 build_mosaic(
-	input_path="rpi.jpg",
-	output_path="rpi_mosaic.png",
-	collection_path="/home/ferran/mosaic/",
+	input_path=inputpicture,
+	output_path=outputmosaic,
+	collection_path=thumbspicturefolder,
 	zoom=4,
-	thumb_size=50,
+	thumb_size=thumbnail_size[0],
 	fuzz=20,
 	new_colormap=False
 )
