ad2d8f2f9ad019e5019bc4d57d4d8649a0cde353
ferriman <ferri.fc@gmail.com>
2015-03-17 15:52:55 +0100

    path bug solved

--- pythonscripts/photo_mosaic_generator.py
+++ pythonscripts/photo_mosaic_generator.py
@@ -8,7 +8,7 @@ import glob
 from pymos.core import build_mosaic
 
 
-filelist= glob.glob("/home/ferran/DCIM/Camera/*.jpg")
+filelist= glob.glob("/home/ferran/nexus5-last/DCIM/Camera/*.jpg")
 thumbnail_size = (100, 100)
 
 # Loop through all provided arguments
@@ -26,14 +26,16 @@ for i in range(1, len(filelist)):
     image = image.resize(thumbnail_size, Image.ANTIALIAS)
     
     # Split our original filename into name and extension
-    (name, extension) = os.path.splitext(filepath)
+    (file_path, extension) = os.path.splitext(filepath)
+    file_path_split=file_path.split('/')
+    print 'Converting /home/ferran/mosaic/'+file_path_split[len(file_path_split)-1]+'.jpg'
     
     # Save the thumbnail as "(original_name)_thumb.jpg"
-    image.save('/home/ferran/mosaic/'+name+'.jpg')
-
+    image.save('/home/ferran/mosaic/'+file_path_split[len(file_path_split)-1]+'.jpg')
+print "Building photo mosaic..."
 build_mosaic(
-	input_path="orignal_image.jpg",
-	output_path="output_mosaic.png",
+	input_path="rpi.jpg",
+	output_path="rpi_mosaic.png",
 	collection_path="/home/ferran/mosaic/",
 	zoom=4,
 	thumb_size=50,
