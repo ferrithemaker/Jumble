1c895115eca1fb2c0a8fd236a75afa84325fd343
ferran <ferri.fc@gmail.com>
2015-07-12 08:48:55 +0000

    barometer update

--- barometric-display/pressuredisplay.py
+++ barometric-display/pressuredisplay.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-# to run this script we need to get the Adafruit BMP085 and the Adafruit 7Segment modules
-
 import time
 import datetime
 import Adafruit_BMP.BMP085 as BMP085
@@ -20,11 +18,11 @@ presure=sensor.read_pressure()/100;
 # Continually update the time on a 4 char, 7-segment display
 while(True):
   presure=sensor.read_pressure()/100;
-  segment.writeDigit(0, int(str(presure)[0]))     
+  print(presure)
+  segment.writeDigit(0, int(str(presure)[0]),True)     
   segment.writeDigit(1, int(str(presure)[1]))          
   segment.writeDigit(3, int(str(presure)[2]))  
-  segment.writeDigit(4, int(str(presure)[3]))        
-  #segment.setColon(second % 2)            
+  segment.writeDigit(4, int(str(presure)[3]))                    
   # Wait one second
   time.sleep(1)
 
