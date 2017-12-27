6c59ddb8b2860dc9487763c902e2a8c002a27a9d
Ferran <ferri.fc@gmail.com>
2017-12-27 15:39:19 +0100

    slider tactil

--- lifebox/webapp/postdata.php
+++ lifebox/webapp/postdata.php
@@ -1,6 +1,6 @@
 <?php
   $lifeboxfile = fopen("lifeboxdata", "w") or die("Unable to open file!");
-  $string =  $_POST['vsp1-1'] . "|" . $_POST['vsp1-2'] . "|" . $_POST['vsp1-3'] . "|" . $_POST['vsp1-4'] . "|" . $_POST['vsp1-5'] . "|" . $_POST['vsp1-6'] . "|" . $_POST['vsp1-7'] . "|" . $_POST['vsp1-8'] . "|" . $_POST['vsp2-1'] . "|" . $_POST['vsp2-2'] . "|" . $_POST['vsp2-3'] . "|" . $_POST['vsp2-4'] . "|" . $_POST['vsp2-5'] . "|" . $_POST['vsp2-6'] . "|" . $_POST['vsp2-7'] . "|" . $_POST['vsp2-8'] . "|" . $_POST['vsp3-1'] . "|" . $_POST['vsp3-2'] . "|" . $_POST['vsp3-3'] . "|" . $_POST['vsp3-4'] . "|" . $_POST['vsp3-5'];
+  $string =  $_POST['amount-sp1-1'] . "|" . $_POST['amount-sp1-2'] . "|" . $_POST['amount-sp1-3'] . "|" . $_POST['amount-sp1-4'] . "|" . $_POST['amount-sp1-5'] . "|" . $_POST['amount-sp1-6'] . "|" . $_POST['amount-sp1-7'] . "|" . $_POST['amount-sp1-8'] . "|" . $_POST['amount-sp2-1'] . "|" . $_POST['amount-sp2-2'] . "|" . $_POST['amount-sp2-3'] . "|" . $_POST['amount-sp2-4'] . "|" . $_POST['amount-sp2-5'] . "|" . $_POST['amount-sp2-6'] . "|" . $_POST['amount-sp2-7'] . "|" . $_POST['amount-sp2-8'] . "|" . $_POST['amount-sp3-1'] . "|" . $_POST['amount-sp3-2'] . "|" . $_POST['amount-sp3-3'] . "|" . $_POST['amount-sp3-4'] . "|" . $_POST['amount-sp3-5'];
   fwrite($lifeboxfile, $string);
   fclose($lifeboxfile);
   header('Location: index.php');
