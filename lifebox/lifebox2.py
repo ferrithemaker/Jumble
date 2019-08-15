c7d934ec0ca3f894f57c70c3aa5a1a969f6152c7
Ferran <ferri.fc@gmail.com>
2019-08-15 16:11:54 +0200

    debug and app mode as default

--- lifebox/lifebox2.py
+++ lifebox/lifebox2.py
@@ -12,18 +12,29 @@ import lifebox_constants as constants
 potData = [0] * 11
 
 # potData[0] Vitality [Plants] A0
+plantsvitality = 0
 # potData[1] Reproduction [Plants] A1
+plantsreproduction = 0
 # potData[2] Generation [Plants] A2
+plantsgeneration = 0
 
-# potData[3] Reproduction [Specie1] A3
+# potData[3] Reproduction [Specie1 (YELLOW)] A3
+sp1reproduction = 0
 # potData[4] Vitality [Specie1] A4
+sp1vitality = 0
 # potData[5] Efficiency [Specie1] A5
+sp1efficency = 0
 # potData[6] Gathering [Specie1] A6
+sp1gathering = 0
 
-# potData[7] Vitality [Specie2] A7
+# potData[7] Vitality [Specie2 (BLUE)] A7
+sp2vitality = 0
 # potData[8] Reproduction [Specie2] A8
+sp2reproduction = 0
 # potData[9] Efficiency [Specie2] A9
+sp2efficency = 0
 # potData[10] Gathering [Specie2] A10
+sp2gathering = 0
 
 
 
@@ -35,9 +46,17 @@ potData = [0] * 11
 def plants_next_iteration(x,y):
 	global plants_individuals
 	global full_matrix_plants_energy
+	global plantsreproduction
+	global plantsvitality
+	global plantsgeneration
 	neighbours = 0
 	# maping plants potData
 	plantsreproduction = map(potData[1],0,1023,0,120)
+	# potData[0] Vitality [Plants] A0
+	plantsvitality = map(potData[0],0,1023,0,120)
+	# potData[2] Generation [Plants] A2
+	plantsgeneration = map(potData[2],0,1023,0,120)
+
 	#if midi_enable:
 	#			midiout.send_message(note_off_white)
 	# adjacent coordinates
@@ -116,9 +135,24 @@ def species_next_iteration(x,y):
 	global specie2_individuals
 	global full_matrix_specie1_energy
 	global full_matrix_specie2_energy
+	global sp1reproduction
+	global sp1vitality
+	global sp1efficency
+	global sp1gathering
+	global sp2reproduction
+	global sp2vitality
+	global sp2efficency
+	global sp2gathering
 	# maping plants potData
 	sp1reproduction = map(potData[3],0,1023,0,80)
-	sp2reproduction = map(potData[8],0,1023,0,80)
+	sp1vitality = map(potData[4],0,1023,0,100)
+	sp1efficency = map(potData[5],0,1023,0,100)
+	sp1gathering = map(potData[6],0,1023,0,100)
+	sp2reproduction = map(potData[8],0,1023,0,100)
+	sp2vitality = map(potData[7],0,1023,0,100)
+	sp2efficency = map(potData[9],0,1023,0,100)
+	sp2gathering = map(potData[10],0,1023,0,100)
+
 	# midi
 	if midi_enable:
 		midiout.send_message(note_off_blue)
@@ -449,12 +483,41 @@ def draw_species(x,y):
 		pygame.draw.circle(screen,green,(((x*2*circle_size)+circle_size)+40,((y*2*circle_size)+circle_size)+40),circle_size,0)
 	if specie1[x][y][0] == 0 and specie2[x][y][0] == 0 and plants[x][y][0] == 0:
 		pygame.draw.circle(screen,constants.BLACK,(((x*2*circle_size)+circle_size)+40,((y*2*circle_size)+circle_size)+40),circle_size,0)
+	if debug == True:
+		deb_prep = textfont.render("plants reproduction: "+str(plantsreproduction),False, constants.MIDGREY, constants.BLACK)
+		deb_pvit = textfont.render("plants vitality: "+str(plantsvitality),False, constants.MIDGREY, constants.BLACK)
+		deb_pgen = textfont.render("plants generation: "+str(plantsgeneration),False, constants.MIDGREY, constants.BLACK)
+		deb_sp1rep = textfont.render("sp1 reproduction: "+str(sp1reproduction),False, constants.MIDGREY, constants.BLACK)
+		deb_sp1vit = textfont.render("sp1 vitality: "+str(sp1vitality),False, constants.MIDGREY, constants.BLACK)
+		deb_sp1eff = textfont.render("sp1 efficiency: "+str(sp1efficency),False, constants.MIDGREY, constants.BLACK)
+		deb_sp1gath = textfont.render("sp1 gathering: "+str(sp1gathering),False, constants.MIDGREY, constants.BLACK)
+		deb_sp2rep = textfont.render("sp2 reproduction: "+str(sp2reproduction),False, constants.MIDGREY, constants.BLACK)
+		deb_sp2vit = textfont.render("sp2 vitality: "+str(sp2vitality),False, constants.MIDGREY, constants.BLACK)
+		deb_sp2eff = textfont.render("sp2 efficiency: "+str(sp2efficency),False, constants.MIDGREY, constants.BLACK)
+		deb_sp2gath = textfont.render("sp2 gathering: "+str(sp2gathering),False, constants.MIDGREY, constants.BLACK)	
+		screen.blit(deb_pvit,(400,50))
+		screen.blit(deb_prep,(400,90))
+		screen.blit(deb_pgen,(400,130))
+		screen.blit(deb_sp1rep,(400,170))
+		screen.blit(deb_sp1vit,(400,210))
+		screen.blit(deb_sp1eff,(400,250))
+		screen.blit(deb_sp1gath,(400,290))
+		screen.blit(deb_sp2rep,(400,330))
+		screen.blit(deb_sp2vit,(400,370))
+		screen.blit(deb_sp2eff,(400,410))
+		screen.blit(deb_sp2gath,(400,450))
+
 
 def readPotDatafromFile(stop):
 	global potData
 	while not stop:
-		file = open("/var/www/html/lifeboxdata", "r")
-		potData = file.read().split("|")
+		file = open("./controllerdata", "r")
+		index = 0
+		for x in file:
+			#print(x)
+			potData[index] = int(x)
+			index = index + 1
+		file.close()
 		time.sleep(1)
 		
 def readPotDatafromArduino(stop):
@@ -466,32 +529,35 @@ def readPotDatafromArduino(stop):
 		board.analog[i].enable_reporting()
 	while not stop:
 		for i in range (0,11):
-			if board.analog[i].read() is not None:
-				potData[i] = int(board.analog[i].read() * 1023)
-				if debug == True:
-					print ("Analog input ",i," > ",int(potData[i]))
+			time.sleep(0.2)
+			prevalue = potData[i]
+			potData[i] = board.analog[i].read()
+			if potData[i] is not None:
+				potData[i] = potData[i] * 1023
 			else:
-				#print("Error analog read ", i)
-				pass
+				potData[i] = prevalue
+				#if debug == True:
+					#print ("Analog input ",i," > ",int(potData[i]))
+
 		# map the values if needed
 		#potData[x] = map(potData[x],0,1023,low,high)
-		time.sleep(1)
+		#time.sleep(1)
 	#print("Arduino connection error! Change to app mode.")
 	#os._exit(1)
 
 midi_enable = 0 # play generative sound through midi out (under development)
 graph_mode = 0 # show graphs
 real_mode = 1 # respawn control
-app_mode = 0 # via web / app or manual controller
+app_mode = True # via web / app or manual controller
 gradient_mode = 1 # individual fade in / out linked to energy
-fullscreen_mode = 1
+fullscreen_mode = True
 fullscreen_graph = 0
-debug = False
+debug = True
 rf = 2 # reduction factor
 
 # run thread
 stop = False
-if app_mode == 0:
+if app_mode == False:
 	t = threading.Thread(target=readPotDatafromArduino,args=(stop,))
 else:
 	t = threading.Thread(target=readPotDatafromFile,args=(stop,))
@@ -518,7 +584,7 @@ if midi_enable:
 
 pygame.display.set_caption('LifeBox')
 
-if fullscreen_mode == 0:
+if fullscreen_mode == False:
 	screen = pygame.display.set_mode((1000,600))
 	matrix_size_x = 32
 	matrix_size_y = 32
@@ -532,6 +598,7 @@ else:
 	circle_size = 7
 
 textfont = pygame.font.SysFont('arial',30)
+debugfont = pygame.font.SysFont('arial',15)
 
 
 # fps management
@@ -648,7 +715,7 @@ while (True):
 			pygame.draw.line(screen,constants.LIGHTGREY,(700+x,350-int(plantsEnergyArray[x]/(5000*rf))),(700+x,350-int(plantsEnergyArray[x]/(5000*rf))))
 
 	# transparent graph for fullscreen mode
-	if fullscreen_graph == 1 and fullscreen_mode == 1:
+	if fullscreen_graph == 1 and fullscreen_mode == True:
 		# generate fullscreen graphs
 		for x in range(1,200):
 				specie1IndividualsArray[x-1] = specie1IndividualsArray[x]
--- lifebox/readanalog.py
+++ lifebox/readanalog.py
@@ -1,31 +1,20 @@
-import pyfirmata
+from pyfirmata import ArduinoMega, util
  
-# Definition of the analog pin
-PINS = (0, 1, 2, 3, 4)
- 
-# Creates a new board 
-board = pyfirmata.Arduino("/dev/ttyACM0")
-#print "Setting up the connection to the board ..."
-it = pyfirmata.util.Iterator(board)
+
+board = ArduinoMega('/dev/ttyACM0')
+it = util.Iterator(board)
 it.start()
  
 # Start reporting for defined pins
-for pin in PINS:
+for pin in range (0,11):
     board.analog[pin].enable_reporting()
 while(True):
 	board.pass_time(2)
 	f = open('controllerdata','w')
-	for pin in PINS:
-                #print "Analog Pin %i : %s" % (pin, board.analog[pin].read())
-			f.write(str(board.analog[pin].read()))
-			f.write('\n')
+	for pin in range (0,11):
+		print ("Analog Pin %i : %s" % (pin, int(board.analog[pin].read()*1023)))
+		f.write(str(int(board.analog[pin].read()*1023)))
+		f.write('\n')
 	f.close()
- 
-# Loop for reading the input. Duration approx. 10 s
-#for i in range(1, 11):
-#	print "\nValues after %i second(s)" % i
-#	for pin in PINS:
-#		print "Analog Pin %i : %s" % (pin, board.analog[pin].read())
-#	board.pass_time(1)	
 
 board.exit()
