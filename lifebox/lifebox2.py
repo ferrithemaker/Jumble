23315cb1b6308198364eefe1fd3f402e98967abe
Ferran <ferri.fc@gmail.com>
2019-08-15 22:48:06 +0200

    values fine tunning

--- lifebox/lifebox2.py
+++ lifebox/lifebox2.py
@@ -53,9 +53,9 @@ def plants_next_iteration(x,y):
 	# maping plants potData
 	plantsreproduction = map(potData[1],0,1023,0,120)
 	# potData[0] Vitality [Plants] A0
-	plantsvitality = map(potData[0],0,1023,0,120)
+	plantsvitality = map(potData[0],0,1023,0,100)
 	# potData[2] Generation [Plants] A2
-	plantsgeneration = map(potData[2],0,1023,0,120)
+	plantsgeneration = map(potData[2],0,1023,0,100)
 
 	#if midi_enable:
 	#			midiout.send_message(note_off_white)
@@ -90,17 +90,17 @@ def plants_next_iteration(x,y):
 	if plants[x][y][0] == 0 and plants[xp][yp][0] > 0:
 		neighbours += 1
 	# if too old, plant dies
-	if plants[x][y][0] >= constants.PLANTS_LIFE_EXPECTANCY + int(potData[0]):
+	if plants[x][y][0] >= constants.PLANTS_LIFE_EXPECTANCY + int(plantsvitality):
 		plants[x][y][0] = 0
 		plants[x][y][1] = 0
 	# if no energy, plant dies
-	if plants[x][y][0] > 0 and plants[x][y][0] < constants.PLANTS_LIFE_EXPECTANCY + int(potData[0]) and plants[x][y][1] <= 0:
+	if plants[x][y][0] > 0 and plants[x][y][0] < (constants.PLANTS_LIFE_EXPECTANCY + int(plantsvitality)) and plants[x][y][1] <= 0:
 		plants[x][y][0] = 0
 		plants[x][y][1] = 0
 	# plant growsplants
-	if plants[x][y][0]>0 and plants[x][y][0] < constants.PLANTS_LIFE_EXPECTANCY + int(potData[0]):
+	if plants[x][y][0]>0 and plants[x][y][0] < constants.PLANTS_LIFE_EXPECTANCY + int(plantsvitality):
 		plants[x][y][0] += 1
-		plants[x][y][1] = plants[x][y][1] + constants.PLANTS_ENERGY_BASE_PER_CYCLE
+		plants[x][y][1] = plants[x][y][1] + constants.PLANTS_ENERGY_BASE_PER_CYCLE + int(plantsgeneration)
 		plants_individuals += 1
 		full_matrix_plants_energy += plants[x][y][1]
 	# plant reproduction
@@ -112,7 +112,7 @@ def plants_next_iteration(x,y):
 		random_number = random.randint(1,randomborn)
 		if random_number == 1:
 			plants[x][y][0] = 1
-			plants[x][y][1] = constants.PLANTS_ENERGY_BASE_PER_CYCLE + int(potData[2])
+			plants[x][y][1] = constants.PLANTS_ENERGY_BASE_PER_CYCLE + int(plantsgeneration)
 			plants_individuals += 1
 			#if midi_enable:
 			#	midiout.send_message(note_on_white)
@@ -122,7 +122,7 @@ def plants_next_iteration(x,y):
 		random_number = random.randint(1,constants.PLANTS_RANDOM_BORN_CHANCES)
 		if random_number == 1:
 			plants[x][y][0] = 1
-			plants[x][y][1] = constants.PLANTS_ENERGY_BASE_PER_CYCLE + int(potData[2])
+			plants[x][y][1] = constants.PLANTS_ENERGY_BASE_PER_CYCLE + int(plantsgeneration)
 			plants_individuals += 1
 			full_matrix_plants_energy += plants[x][y][1]
 			#if midi_enable:
@@ -144,14 +144,14 @@ def species_next_iteration(x,y):
 	global sp2efficency
 	global sp2gathering
 	# maping plants potData
-	sp1reproduction = map(potData[3],0,1023,0,80)
+	sp1reproduction = map(potData[3],0,1023,0,120)
 	sp1vitality = map(potData[4],0,1023,0,100)
-	sp1efficency = map(potData[5],0,1023,0,100)
-	sp1gathering = map(potData[6],0,1023,0,100)
-	sp2reproduction = map(potData[8],0,1023,0,100)
+	sp1efficency = map(potData[5],0,1023,0,110)
+	sp1gathering = map(potData[6],0,1023,0,130)
+	sp2reproduction = map(potData[8],0,1023,0,120)
 	sp2vitality = map(potData[7],0,1023,0,100)
-	sp2efficency = map(potData[9],0,1023,0,100)
-	sp2gathering = map(potData[10],0,1023,0,100)
+	sp2efficency = map(potData[9],0,1023,0,110)
+	sp2gathering = map(potData[10],0,1023,0,130)
 
 	# midi
 	if midi_enable:
@@ -217,9 +217,9 @@ def species_next_iteration(x,y):
 		# try to eat
 		if plants[x][y][1] > 0:
 			total_energy=0
-			if plants[x][y][1] > constants.SPECIE1_MAX_ENERGY_RECOLECTED_PER_CYCLE + int(potData[6]):
-				total_energy = constants.SPECIE1_MAX_ENERGY_RECOLECTED_PER_CYCLE + int(potData[6])
-				plants[x][y][1] = plants[x][y][1] - (constants.SPECIE1_MAX_ENERGY_RECOLECTED_PER_CYCLE + int(potData[6]))
+			if plants[x][y][1] > constants.SPECIE1_MAX_ENERGY_RECOLECTED_PER_CYCLE + int(sp1gathering):
+				total_energy = constants.SPECIE1_MAX_ENERGY_RECOLECTED_PER_CYCLE + int(sp1gathering)
+				plants[x][y][1] = plants[x][y][1] - (constants.SPECIE1_MAX_ENERGY_RECOLECTED_PER_CYCLE + int(sp1gathering))
 			else:
 				total_energy = plants[x][y][1]
 				plants[x][y][1] = 0
@@ -227,7 +227,7 @@ def species_next_iteration(x,y):
 			#print "("+str(x)+","+str(y)+") eats"
 		# grow and decrease energy
 		specie1[x][y][0] += 1
-		specie1[x][y][1] = specie1[x][y][1] - (constants.SPECIE1_ENERGY_NEEDED_PER_CYCLE  + int(potData[5]))
+		specie1[x][y][1] = specie1[x][y][1] - (constants.SPECIE1_ENERGY_NEEDED_PER_CYCLE  - int(sp1efficency))
 		#print "("+str(x)+","+str(y)+") grows"
 		# die if no energy
 		if specie1[x][y][1] < 0:
@@ -235,14 +235,14 @@ def species_next_iteration(x,y):
 			specie1[x][y][0] = 0
 			#print "("+str(x)+","+str(y)+") dies"
 		# try to replicate
-		if specie1[x][y][1] > constants.SPECIE1_ENERGY_TO_REPLICATE and specie1[x][y][2] == 0:
+		if specie1[x][y][1] > constants.SPECIE1_ENERGY_TO_REPLICATE:
 			available_spots = [0 for numspots in range(8)]
 			pos=0
 			if int(sp1reproduction) > 0:
 				if constants.SPECIE1_NEARBORN_CHANCES - int(sp1reproduction) < 2:
 					randomborn = 2
 				else:
-					randomborn = constants.SPECIE1_NEARBORN_CHANCES - int(sp1reproduction)
+					randomborn = map(constants.SPECIE1_NEARBORN_CHANCES - int(sp1reproduction),2,120,2,20)
 				random_number = random.randint(1,randomborn)
 				if specie1[xm][y][0] == 0:
 					available_spots[pos] = 1
@@ -271,6 +271,7 @@ def species_next_iteration(x,y):
 				if pos > 0:
 					rand_pos=random.randint(0,pos-1)
 					if random_number == 1:
+						specie1[x][y][1] = specie1[x][y][1] - constants.SPECIE1_ENERGY_TO_REPLICATE
 						#print "ready to reproduce at ("+str(xm)+","+str(ym)+") - ("+str(xp)+","+str(yp)+") - center ("+str(x)+","+str(y)+")"
 						if available_spots[rand_pos] == 1:
 							specie1[xm][y][0] = 1
@@ -308,7 +309,7 @@ def species_next_iteration(x,y):
 						if midi_enable:
 							midiout.send_message(note_on_yellow)
 		# die if too old
-		if specie1[x][y][0] > constants.SPECIE1_LIFE_EXPECTANCY + int(potData[4]):
+		if specie1[x][y][0] > constants.SPECIE1_LIFE_EXPECTANCY + int(sp1vitality):
 			specie1[x][y][1] = 0
 			specie1[x][y][0] = 0
 			#print "("+str(x)+","+str(y)+") dies"
@@ -316,7 +317,7 @@ def species_next_iteration(x,y):
 		specie1_individuals += 1
 		full_matrix_specie1_energy += specie1[x][y][1]
 	# if no individual is alive, random born to avoid extintion
-	if specie1[x][y][0] == 0 and specie1_neighbours==0 and specie1[x][y][2] == 0 and ((specie1_last_individuals == 0 and specie1_individuals == 0 and real_mode == 1) or real_mode == 0):
+	if specie1[x][y][0] == 0 and specie1_neighbours==0 and ((specie1_last_individuals == 0 and specie1_individuals == 0 and real_mode == 1) or real_mode == 0):
 		random_number = random.randint(1,constants.SPECIE1_RANDOM_BORN_CHANCES)
 		if random_number==1:
 			specie1[x][y][0] = 1
@@ -334,9 +335,9 @@ def species_next_iteration(x,y):
 		# try to eat
 		if plants[x][y][1] > 0:
 			total_energy=0
-			if plants[x][y][1] > constants.SPECIE2_MAX_ENERGY_RECOLECTED_PER_CYCLE + int(potData[10]):
-				total_energy = constants.SPECIE2_MAX_ENERGY_RECOLECTED_PER_CYCLE + int(potData[10])
-				plants[x][y][1] = plants[x][y][1] - (constants.SPECIE2_MAX_ENERGY_RECOLECTED_PER_CYCLE + int(potData[10]))
+			if plants[x][y][1] > constants.SPECIE2_MAX_ENERGY_RECOLECTED_PER_CYCLE + int(sp2gathering):
+				total_energy = constants.SPECIE2_MAX_ENERGY_RECOLECTED_PER_CYCLE + int(sp2gathering)
+				plants[x][y][1] = plants[x][y][1] - (constants.SPECIE2_MAX_ENERGY_RECOLECTED_PER_CYCLE + int(sp2gathering))
 			else:
 				total_energy = plants[x][y][1]
 				plants[x][y][1] = 0
@@ -344,7 +345,7 @@ def species_next_iteration(x,y):
 			#print "("+str(x)+","+str(y)+") eats"
 		# grow and decrease energy
 		specie2[x][y][0] += 1
-		specie2[x][y][1] = specie2[x][y][1] - (constants.SPECIE2_ENERGY_NEEDED_PER_CYCLE  + int(potData[9]))
+		specie2[x][y][1] = specie2[x][y][1] - (constants.SPECIE2_ENERGY_NEEDED_PER_CYCLE - int(sp2efficency))
 		#print "("+str(x)+","+str(y)+") grows"
 		# die if no energy
 		if specie2[x][y][1] < 0:
@@ -352,14 +353,14 @@ def species_next_iteration(x,y):
 			specie2[x][y][0] = 0
 			#print "("+str(x)+","+str(y)+") dies"
 		# try to replicate
-		if specie2[x][y][1] > constants.SPECIE2_ENERGY_TO_REPLICATE and specie2[x][y][2] == 0:
+		if specie2[x][y][1] > constants.SPECIE2_ENERGY_TO_REPLICATE:
 			available_spots = [0 for numspots in range(8)]
 			pos=0
 			if int(sp2reproduction) > 0:
 				if constants.SPECIE2_NEARBORN_CHANCES - int(sp2reproduction) < 2:
 					randomborn = 2
 				else:
-					randomborn = constants.SPECIE2_NEARBORN_CHANCES - int(sp2reproduction)
+					randomborn = map(constants.SPECIE2_NEARBORN_CHANCES - int(sp2reproduction),2,120,2,20)
 				random_number = random.randint(1,randomborn)
 				if specie2[xm][y][0] == 0:
 					available_spots[pos] = 1
@@ -388,6 +389,7 @@ def species_next_iteration(x,y):
 				if pos > 0:
 					rand_pos=random.randint(0,pos-1)
 					if random_number == 1:
+						specie2[x][y][1] = specie2[x][y][1] - constants.SPECIE2_ENERGY_TO_REPLICATE
 						#print "ready to reproduce at ("+str(xm)+","+str(ym)+") - ("+str(xp)+","+str(yp)+") - center ("+str(x)+","+str(y)+")"
 						if available_spots[rand_pos] == 1:
 							specie2[xm][y][0] = 1
@@ -425,7 +427,7 @@ def species_next_iteration(x,y):
 						if midi_enable:
 							midiout.send_message(note_on_blue)
 		# die if too old
-		if specie2[x][y][0] > constants.SPECIE2_LIFE_EXPECTANCY + int(potData[7]):
+		if specie2[x][y][0] > constants.SPECIE2_LIFE_EXPECTANCY + int(sp2vitality):
 			specie2[x][y][1] = 0
 			specie2[x][y][0] = 0
 			#print "("+str(x)+","+str(y)+") dies"
@@ -450,22 +452,22 @@ def map(x,in_min,in_max,out_min,out_max):
 
 def draw_species(x,y):
 	if gradient_mode == 1:
-		if plants[x][y][1]>200 * rf:
-			green = (0,200,0)
+		if plants[x][y][1]>12000:
+			green = (0,240,0)
 		else:
-			green = (0,int(plants[x][y][1]/rf),0)
-		if specie1[x][y][1]>200 * rf:
-			yellow = (200,200,0)
+			green = (0,map(plants[x][y][1],0,12000,0,240),0)
+		if specie1[x][y][1] > 4000:
+			yellow = (240,240,0)
 		else:
-			yellow = (int(specie1[x][y][1]/rf),int(specie1[x][y][1]/rf),0)
-		if specie2[x][y][1]>200 * rf:
-			blue = (0,0,200)
+			yellow = (map(specie1[x][y][1],0,4000,50,240),map(specie1[x][y][1],0,4000,50,240),0)
+		if specie2[x][y][1] > 4000:
+			blue = (0,0,240)
 		else:
-			blue = (0,0,int(specie2[x][y][1]/rf))
-		if specie1[x][y][1]+specie2[x][y][1] > 200 * rf:
-			magenta = (200,0,200)
+			blue = (0,0,map(specie2[x][y][1],0,4000,50,240))
+		if specie1[x][y][1]+specie2[x][y][1] > 4000:
+			magenta = (240,0,240)
 		else:
-			magenta = (int(specie1[x][y][1]/rf)+int(specie2[x][y][1]/rf),0,int((specie1[x][y][1]+specie2[x][y][1])/rf))
+			magenta = (map(specie1[x][y][1],0,4000,50,240)+map(specie2[x][y][1],0,4000,50,240),0,map(specie1[x][y][1],0,4000,50,240)+map(specie2[x][y][1],0,4000,50,240))
 	else:
 		white = (255,255,255)
 		green = (0,255,0)
@@ -551,8 +553,8 @@ real_mode = 1 # respawn control
 app_mode = True # via web / app or manual controller
 gradient_mode = 1 # individual fade in / out linked to energy
 fullscreen_mode = True
-fullscreen_graph = 0
-debug = True
+fullscreen_graph = False
+debug = False
 rf = 2 # reduction factor
 
 # run thread
@@ -715,7 +717,7 @@ while (True):
 			pygame.draw.line(screen,constants.LIGHTGREY,(700+x,350-int(plantsEnergyArray[x]/(5000*rf))),(700+x,350-int(plantsEnergyArray[x]/(5000*rf))))
 
 	# transparent graph for fullscreen mode
-	if fullscreen_graph == 1 and fullscreen_mode == True:
+	if fullscreen_graph == True and fullscreen_mode == True:
 		# generate fullscreen graphs
 		for x in range(1,200):
 				specie1IndividualsArray[x-1] = specie1IndividualsArray[x]
--- lifebox/lifebox_constants.py
+++ lifebox/lifebox_constants.py
@@ -15,41 +15,37 @@ LIGHTGREY = (200,200,200,50)
 #plants
 
 PLANTS_LIFE_EXPECTANCY = 40 #A0
-PLANTS_RANDOM_BORN_CHANCES = 5100 # fixed
-PLANTS_NEARBORN_CHANCES = 150 #A1
-PLANTS_RANDOM_DIE_CHANCES = 2 # not used
-PLANTS_ENERGY_BASE_PER_CYCLE = 100 #A2
+PLANTS_RANDOM_BORN_CHANCES = 5000 # fixed
+PLANTS_NEARBORN_CHANCES = 120 #A1
+PLANTS_ENERGY_BASE_PER_CYCLE = 30 #A2
 # Each mana invidivual generates a defined amout of energy per cycle. This energy is gathered by the species. Low energy generation means a poor enviroment for the species to survive, and high energy generation a rich one.
+
 
 #yellow
 SPECIE1_LIFE_EXPECTANCY = 40 #A4
 # Life expectancy is an statistical measure of the average time an organism is expected to live. Once a pixelic entity becomes stable, life expectancy determines how many reiterations does the pixel survive.
-SPECIE1_RANDOM_BORN_CHANCES = 5100 
+SPECIE1_RANDOM_BORN_CHANCES = 5000 
 # Fixed
 # Parthenogesis is a rare trait among species which allows them to reproduce without mating. The species inside LifeBox! can reproduce in a similar way. In case they achieve it, offspring is randomly populated inside the grid. 
 # Setting this variable with a high value means less chances to reproduce that way. Otherwise, if user choose to reduce this value, parthenogenesis is more probable to happen
-SPECIE1_NEARBORN_CHANCES = 25 #A3
+SPECIE1_NEARBORN_CHANCES = 120 #A3
 # When two pixelic entities of the same specie are adjacent to each other, they can reproduce. This variable determines the reproduction chances, so a higher value means a higher chances to survive.
-SPECIE1_RANDOM_DIE_CHANCES = 2
-# NOT USED
-# As in real life, LifeBox! pixelic species can die before reaching their life expectancy. Setting a low value, will allow pixelic entities to arrive at their expected life time. While a higher value will reduce seriously their chances to survive until the expected average life time.
 SPECIE1_ENERGY_BASE = 250
-# NOT USED
+# fixed
 # Every spices has a defined base level of energy when it borns, this base level will condition the chances of survival at very first stages of its life.
-SPECIE1_ENERGY_NEEDED_PER_CYCLE = 50 #A5
+SPECIE1_ENERGY_NEEDED_PER_CYCLE = 120 #A5
 # This parameter defines the species amount of energy consumtion at each iteration. Higher values mean that the species needs more energy per iteration cycle, meaning less efficiency.
-SPECIE1_MAX_ENERGY_RECOLECTED_PER_CYCLE = 100 #A6
+SPECIE1_MAX_ENERGY_RECOLECTED_PER_CYCLE = 20 #A6
 # As the previous parameter defines the efficiency of energy consumtion, this one defines the efficiency of energy gathering from the mana. Higher values mean more gathering efficiency.
-SPECIE1_ENERGY_TO_REPLICATE = 150
-# NOT USED
+SPECIE1_ENERGY_TO_REPLICATE = 200
+# fixed
 # To allow the species replication, each individual needs to exceed an energy threshold, the minimum amount of energy needed to be able to reproduce itself. Higher values mean higher threshold.
 
 #blue
 SPECIE2_LIFE_EXPECTANCY = 40 #A7
-SPECIE2_RANDOM_BORN_CHANCES = 5100 # fixed
-SPECIE2_NEARBORN_CHANCES = 30 #A8
-SPECIE2_RANDOM_DIE_CHANCES = 2 # not used
-SPECIE2_ENERGY_BASE = 250 # not used
-SPECIE2_ENERGY_NEEDED_PER_CYCLE = 50 #A9
-SPECIE2_MAX_ENERGY_RECOLECTED_PER_CYCLE = 100 #A10
-SPECIE2_ENERGY_TO_REPLICATE = 150 # not used
+SPECIE2_RANDOM_BORN_CHANCES = 5000 # fixed
+SPECIE2_NEARBORN_CHANCES = 120 #A8
+SPECIE2_ENERGY_BASE = 250 # fixed
+SPECIE2_ENERGY_NEEDED_PER_CYCLE = 120 #A9
+SPECIE2_MAX_ENERGY_RECOLECTED_PER_CYCLE = 20 #A10
+SPECIE2_ENERGY_TO_REPLICATE = 200 # fixed
