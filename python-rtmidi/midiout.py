c043e4c5dd5a47d78e1fbce1f1f8e261bc7b6775
ferran <ferri.fc@gmail.com>
2020-02-18 21:18:06 +0100

    lifebox generative sound test1

--- python-rtmidi/midiout.py
+++ python-rtmidi/midiout.py
@@ -1,13 +1,30 @@
 import time
 import rtmidi
+import random
 
+def chordGenerator():
+    startValue = random.randint(50,60)
+    return ([startValue,startValue+4,startValue+7])
+
+def stopAllSounds():
+    for i in range(50,60):
+        midiout.send_message([0x80,i,30])
 midiout = rtmidi.MidiOut()
 available_ports = midiout.get_ports()
 print (available_ports)
+print (midiout.get_port_count())
 midiout.open_port(1)
-note_on = [0x90,60,112] # channel 1, note C , velocity 112
-note_off = [0x80,60,0]
-midiout.send_message(note_on)
-time.sleep(0.5)
-midiout.send_message(note_off)
-del midiout
+stopAllSounds()
+
+try:
+    while True:
+        chord = chordGenerator()
+        midiout.send_message([0x90,chord[0],30])
+        midiout.send_message([0x90,chord[1],30])
+        midiout.send_message([0x90,chord[2],30])
+        time.sleep(float(random.randint(3000,5000)/1000))
+        midiout.send_message([0x80,chord[0],0])
+        midiout.send_message([0x80,chord[1],0])
+        midiout.send_message([0x80,chord[2],0])
+except KeyboardInterrupt:
+    del midiout
