54488db753a2774f32e43064cc59a0e4fb5ab1e5
Ferran <ferri.fc@gmail.com>
2020-09-06 20:19:04 +0200

    esp32 servo added

--- ESP32-TimeOfFligth-sensor-VL53L1X/main.cpp
+++ ESP32-TimeOfFligth-sensor-VL53L1X/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
+
+// lifebox new controller testing
 
 /*
 This example shows how to take simple range measurements with the VL53L1X. The
@@ -9,6 +12,15 @@ range readings are in units of mm.
 #include <VL53L1X.h> // PlatformIO VL53L1X library by pololu
 
 VL53L1X sensor;
+
+
+Servo myservo;  // create servo object to control a servo
+// 16 servo objects can be created on the ESP32
+
+int pos = 20;    // variable to store the servo position
+// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
+int servoPin = 25;
+
 
 void setup()
 {
@@ -35,12 +47,24 @@ void setup()
   // inter-measurement period). This period should be at least as long as the
   // timing budget.
   sensor.startContinuous(50);
+
+  // Allow allocation of all timers
+	ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+	myservo.setPeriodHertz(50);    // standard 50 hz servo
+	myservo.attach(servoPin, 1000, 2000);
 }
 
-void loop()
-{
-  Serial.print(sensor.read());
-  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+void loop() {
+myservo.write(pos);
+pos = pos + 60;
+if (pos>180) { pos = 20; }
+Serial.print(sensor.read());
+if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
 
-  Serial.println();
+Serial.println();
+
+delay(500);
 }
