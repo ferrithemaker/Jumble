7d0ca67e25b0932c2464b6374eb73ec21b161efe
Ferran <ferri.fc@gmail.com>
2021-01-25 19:28:58 +0100

    fully and 1st dose

--- R/vaccinationCovid19/app.R
+++ R/vaccinationCovid19/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+require(ggplot2)
 
 covidVaccinationData <- read.csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv', header=TRUE, sep=",")
 covidVaccinationData$real_date <- as.Date(covidVaccinationData$date,format='%Y-%m-%d')
@@ -15,8 +16,13 @@ ui <- fluidPage(
 server <- function(input,output,session) {
     output$graph <- renderPlot({
         covidSubsetByCountry <- subset(covidVaccinationDataNoNA,location==input$country)
-        plot(covidSubsetByCountry$real_date,covidSubsetByCountry$total_vaccinations_per_hundred,type="b",,xlab="Date",ylab="Total vaccionation x 100 people")
-    })
+        #plot(covidSubsetByCountry$real_date,covidSubsetByCountry$total_vaccinations_per_hundred,type="b",,xlab="Date",ylab="Total vaccionation x 100 people")
+        ggplot(covidSubsetByCountry, aes(real_date)) +
+            xlab("Date") + ylab("Vaccinations") +
+            geom_line(aes(y=total_vaccinations_per_hundred, colour="red")) +
+            geom_line(aes(y=people_fully_vaccinated_per_hundred, colour="green")) +
+            scale_color_discrete(name = "Vaccination", labels = c("Fully vaccinated", "1st dose"))
+        })
 }
 
 shinyApp(ui, server)
