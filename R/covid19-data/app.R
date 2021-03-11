e19c6bb3c1e35847ba37584f8279ebba6989e533
Ferran <ferri.fc@gmail.com>
2021-03-11 22:23:10 +0100

    moving average

--- R/covid19-data/app.R
+++ R/covid19-data/app.R
@@ -1,11 +1,12 @@
 library(shiny)
+library(zoo)
 
 covidData <- read.csv('https://opendata.ecdc.europa.eu/covid19/casedistribution/csv', header=TRUE, sep=",")
 covidData$date <- as.Date(covidData$dateRep,format='%d/%m/%Y')
 listOfTerritories <- unique(covidData["countriesAndTerritories"])
 
 ui <- fluidPage(
-  titlePanel(windowTitle="Updated real-time data of Covid-19 cases & deaths",title=h4("Updated real-time data of Covid-19 cases & deaths (weekly data x 100K population)", align="center")),
+  titlePanel(windowTitle="Updated real-time data of Covid-19 cases & deaths",title=h4("Updated real-time data of Covid-19 cases & deaths (daily data x 100K population)", align="center")),
   selectInput("casesordeaths","Cases or deaths:",c("Cases","Deaths")),
   selectInput("country","Select country:",listOfTerritories$countriesAndTerritories),
   submitButton("Update graph", icon("refresh")),
@@ -16,10 +17,16 @@ server <- function(input,output,session) {
   output$graph <- renderPlot({
     covidSubsetByCountry <- subset(covidData,countriesAndTerritories==input$country)
     if (input$casesordeaths=="Cases") {
-      plot(covidSubsetByCountry$date,(covidSubsetByCountry$cases_weekly/covidSubsetByCountry$popData2019)*100000,type="l",xlab="Date",ylab="Weekly cases x 100K population")
+      covidSubsetByCountry <- covidSubsetByCountry[covidSubsetByCountry$cases >= 0,]
+      dailycases <- (covidSubsetByCountry$cases/covidSubsetByCountry$popData2019)*100000
+      dailycasesRM <- zoo::rollmean(dailycases, k = 7, fill = NA)
+      plot(covidSubsetByCountry$date,dailycasesRM,type="l",xlab="Date",ylab="Daily cases x 100K population")
     }
     if (input$casesordeaths=="Deaths") {
-      plot(covidSubsetByCountry$date,(covidSubsetByCountry$deaths_weekly/covidSubsetByCountry$popData2019)*100000,type="l",xlab="Date",ylab="Weekly deaths x 100K population")
+      covidSubsetByCountry <- covidSubsetByCountry[covidSubsetByCountry$deaths >= 0,]
+      dailydeaths <- (covidSubsetByCountry$deaths/covidSubsetByCountry$popData2019)*100000
+      dailydeathsRM <- zoo::rollmean(dailydeaths, k = 7, fill = NA)
+      plot(covidSubsetByCountry$date,dailydeathsRM,type="l",xlab="Date",ylab="Daily deaths x 100K population")
     }
   })
 }
